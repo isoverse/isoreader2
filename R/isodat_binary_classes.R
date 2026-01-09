be7b9c404ea6639b1ab5ee0a8842ea540ba9d745
Sebastian Kopf <seb.kopf@gmail.com>
2026-01-09 09:48:09 -0700

    first few class defintiions

---
+++ R/isodat_binary_classes.R
@@ -0,0 +1,107 @@
+# general class readers ======
+
+# read CData object (complete)
+read_CData <- function(bfile) {
+  # fields
+  data <- bfile |>
+    read_binary_data_list(
+      c(
+        "version" = "int", # const
+        "APP_ID" = "uint16", # +0x04 (enum APP_ID returned by CData::GetOwner, often set to 0x2f = 47 in constructors)
+        "?" = "string", # +0x34 # file name?
+        "?" = "string" # +0x38 # file header?
+      )
+    )
+
+  # additional data for version >= 3
+  if (!is.na(data$version) && data$version >= 3) {
+    # +0x7c holds bit flags with the following meanings
+    # +0x7c & 1 = CData::IsSystemOnly
+    # +0x7c & 2 = CData::BackupFile
+    # +0x7c & 4 = CData::IsDisabled
+    # +0x7c & 8 = CData::IsAdvanced
+    data <- bfile |>
+      read_binary_data_list(data = data, c("flags" = "int"))
+  }
+
+  return(dplyr::as_tibble(data))
+}
+
+
+# read CBlockData object (complete BUT derived classes need to take care of the objects stored in the CBlock array)
+read_CBlockData <- function(bfile) {
+  # parent
+  data <- list(
+    pCData = bfile |> read_CData() |> list()
+  )
+
+  # fields
+  data <- bfile |>
+    read_binary_data_list(
+      data = data,
+      c(
+        "version" = "int", # const
+        "n_objects" = "int" # +0x9C
+      )
+    )
+
+  # array of n (n_objects) CData-dervied child objects at +0x98
+  # these are read with: class CObject object = CArchive::ReadObject(ar, &CData::classCData)
+  # the child objects need to be loaded in the derived classes as CBlockData does not know their exact type
+  return(tibble::as_tibble(data))
+}
+
+# scan class readers =========
+
+# read CScanStorage object
+read_CScanStorage <- function(bfile) {
+  # parent
+
+  data <- list(
+    pCBlockData = bfile |> read_CBlockData() |> list()
+  )
+
+  # check on n_objects stored in CBlockData
+  n_objects <- data$pCBlockData[[1]]$n_objects
+
+  # CBlockData should NOT have any child objects for CScanStorage
+  if (!is.na(n_objects) && n_objects > 0) {
+    bfile |>
+      register_cnd(
+        cli_stop(
+          "unexpectedly encountered {n_objects} CBlockData child objects in CScanStorage"
+        ),
+        pos = bfile$pos
+      )
+  }
+
+  data$version <- bfile |> read_binary_data("int") # const
+  # version check
+  if (!is.na(data$version) && data$version < 4) {
+    bfile |>
+      register_cnd(
+        cli_warn(
+          "untested processing of 4 CString objects in CScanStorage version < 4"
+        ),
+        pos = bfile$pos
+      )
+    # these are discarded (just read for backwards compatibility)
+    bfile |> read_binary_data("string")
+    bfile |> read_binary_data("string")
+    bfile |> read_binary_data("string")
+    bfile |> read_binary_data("string")
+  }
+  # fields
+  data <- bfile |>
+    read_binary_data_list(
+      data = data,
+      c(
+        "?" = "string", # +0xf4
+        "n_points" = "int", # +0x168,
+        "n_traces" = "int" # +0x16c
+      )
+    )
+  # expect next object to be CBinary
+  bfile |> read_CRuntimeClass("CBinary", advance = FALSE)
+  return(dplyr::as_tibble(data))
+}
