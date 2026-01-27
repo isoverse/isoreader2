94b0f1b5b44307f0378fc236321f0c326dfbe3ef
Sebastian Kopf <seb.kopf@gmail.com>
2026-01-27 10:32:46 -0700

    restructure class readers

--- R/isodat_binary_core_classes.R
+++ R/isodat_binary_core_classes.R
@@ -253,63 +253,6 @@ read_CGasConfiguration <- function(bfile) {
   dplyr::as_tibble(data)
 }
 
-
-# scan class readers =========
-
-# read CScanStorage object
-read_CScanStorage <- function(bfile) {
-  # parent
-
-  data <- list(
-    pCBlockData = bfile |> read_CBlockData() |> list()
-  )
-
-  # check on n_objects stored in CBlockData
-  n_objects <- data$pCBlockData[[1]]$n_objects
-
-  # CBlockData should NOT have any child objects for CScanStorage
-  if (!is.na(n_objects) && n_objects > 0) {
-    bfile |>
-      register_cnd(
-        cli_stop(
-          "unexpectedly encountered {n_objects} CBlockData child objects in CScanStorage"
-        ),
-        pos = bfile$pos
-      )
-  }
-
-  data$version <- bfile |> read_binary_data("int") # const
-  # version check
-  if (!is.na(data$version) && data$version < 4) {
-    bfile |>
-      register_cnd(
-        cli_warn(
-          "untested processing of 4 CString objects in CScanStorage version < 4"
-        ),
-        pos = bfile$pos
-      )
-    # these are discarded (just read for backwards compatibility)
-    bfile |> read_binary_data("string")
-    bfile |> read_binary_data("string")
-    bfile |> read_binary_data("string")
-    bfile |> read_binary_data("string")
-  }
-  # fields
-  data <- bfile |>
-    read_binary_data_list(
-      data = data,
-      c(
-        "comment" = "string", # +0xf4
-        "n_points" = "int", # +0x168,
-        "n_traces" = "int" # +0x16c
-      )
-    )
-  # expect next object to be CBinary
-  bfile |> read_CRuntimeClass("CBinary", advance = FALSE)
-  return(dplyr::as_tibble(data))
-}
-
-
 # read CVisualisationData object
 # read CVisualisationData object (covers full CSV including child objects)
 read_CVisualisationData <- function(bfile) {
