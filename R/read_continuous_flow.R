ad2617162dd301b11fc38a332037ebcabcddcf45
Sebastian Kopf <seb.kopf@gmail.com>
2026-05-27 16:21:41 -0600

    draft reader

--- R/read_continuous_flow.R
+++ R/read_continuous_flow.R
@@ -74,3 +74,103 @@ ir_find_continuous_flow <- function(
 
   return(sort(files))
 }
+
+#' Read continuous flow files
+#'
+#' @param file_paths paths to the continuous flow file(s), single value or vector of paths. Use [ir_find_continuous_flow()] to get all continuous flow files in a folder.
+#' @param show_progress whether to show a progress bar, by default always enabled when running interactively e.g. inside Positron or RStudio (and disabled in a notebook), turn off with `show_progress = FALSE`
+#' @param show_problems whether to show problems encountered along the way (rather than just keeping track of them with [ir_get_problems()]). Set to `show_problems = FALSE` to turn off the live printout. Either way, all encountered problems can be retrieved with running [ir_get_problems()] for the returned list
+#' @return a tibble data frame where each row holds the file path and nested tibbles of datasets extracted from the continuous flow files. Use [orbi_aggregate_raw()] to aggregate data safely across files.
+#' @export
+ir_read_continuous_flow <- function(
+  file_paths,
+  show_progress = rlang::is_interactive(),
+  show_problems = TRUE
+) {
+  # keep track of current env to anchor progress bars
+  root_env <- current_env()
+
+  # safety checks
+  check_arg(
+    file_paths,
+    !missing(file_paths) &&
+      is_character(file_paths) &&
+      length(file_paths) > 0,
+    "must be at least one file path"
+  )
+
+  # all directories?
+  if (all(dir.exists(file_paths))) {
+    cli_abort(
+      c(
+        "{?this/these} path{?s} ({.file {file_paths}}) {?is a/are} director{?y/ies}, not {?a /}raw file{?s}",
+        "i" = "did you mean to run {.strong ir_find_continuous_flow()} instead?"
+      )
+    )
+  }
+
+  # check which files have .json extracts already
+  file_paths_info <- tibble(
+    file_path = gsub("\\.json$", "", file_paths),
+    has_json = file.exists(file_path |> paste0(".json"))
+  )
+
+  # initialize progress bar
+  start <- start_info(
+    "preparing to read {length(file_paths)} continuous flow file{?s}",
+    pb_total = nrow(file_paths_info),
+    show_progress = show_progress,
+    .env = root_env
+  )
+
+  # empty metadata
+  empty_meta <- tibble::tibble(
+    reader_version = NA_character_,
+    file_type = NA_character_,
+    file_size_bytes = NA_integer_,
+    complete = NA
+  )
+
+  # fetch metadata from json files
+  file_paths_info <- file_paths_info |>
+    dplyr::mutate(
+      meta = purrr::map2(
+        .data$file_path,
+        .data$has_json,
+        function(fp, has_json) {
+          if (!has_json) {
+            # no json file
+            return(empty_meta)
+          }
+          out <- read_json_meta(paste0(fp, ".json")) |> try_catch_cnds()
+          if (is.null(out$result)) {
+            # something went wrong reading the metadata --> reread
+            return(empty_meta)
+          }
+          return(out$result)
+        }
+      )
+    ) |>
+    tidyr::unnest(.data$meta)
+
+  finish_info(
+    "read {nrow(file_paths_info)} continuous flow file{?s}",
+    start = start,
+    #conditions = all_conditions,
+    show_conditions = show_problems,
+    .env = root_env
+  )
+
+  return(file_paths_info)
+}
+
+# extract meta from a single JSON file
+read_json_meta <- function(json_path) {
+  meta <- RcppSimdJson::fload(json_path, query = "/meta")
+  tibble::tibble(
+    reader_version = meta$reader_version,
+    file_type = meta$file_type,
+    file_size_bytes = meta$file_size_bytes,
+    complete = meta$complete
+  )
+}
