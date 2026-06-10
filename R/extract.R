0368c06d32022aace4c93651b1955e91827dd596
Sebastian Kopf <seb.kopf@gmail.com>
2026-06-09 23:11:56 -0600

    require version 0.2.1 of isoextract

--- R/extract.R
+++ R/extract.R
@@ -18,7 +18,7 @@ ir_check_isoextract <- function(
   install_if_missing = !on_cran(),
   reinstall_if_outdated = !on_cran(),
   reinstall_always = FALSE,
-  min_version = "0.2.0",
+  min_version = "0.2.1",
   show_version = TRUE,
   source = paste0(
     "https://github.com/isoverse/IsofileExtractor/releases/download/isoextract-v",
